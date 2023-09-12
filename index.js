6332f13c85f227ebfbf82e25c8ee1fafe5ad1229
Oyeniran Al-Ameen Omotola <Hamyn98@gmail.com>
2023-09-12 12:33:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function printUserDetails(first_name,last_name,age){
+    let userDetails = 'I am ' + first_name + ' ' + last_name + ' I am ' + age + 'years old'
+    console.log(userDetails);
+}
+printUserDetails('Rasak','Damilare','40')
+printUserDetails('oyeniran','Omotola','30')
+
+function multiplyTwoNumbers(a,b){
+    let twoNumbers = 'The product of 2 and 3 is ' + a * b 
+    console.log(twoNumbers);
+}
+multiplyTwoNumbers(2,3)
